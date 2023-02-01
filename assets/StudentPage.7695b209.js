23941702a1f4ca130500240a536bb0a9d385da57
ccosse <ccosse@gmail.com>
2023-01-31 23:02:35 -0800

    update

---
+++ assets/StudentPage.7695b209.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,af as a}from"./index.83c72abb.js";const n={};function r(c,o){return t(),a("h1",null,"StudentPage")}var s=e(n,[["render",r]]);export{s as default};
---
+++ assets/TeacherPage.8c14eff9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,af as r}from"./index.83c72abb.js";const c={};function n(o,t){return a(),r("h1",null,"TeacherPage")}var s=e(c,[["render",n]]);export{s as default};

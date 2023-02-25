177c93327ea0f931a7511493e2bbca86527a9f8d
ccosse <ccosse@gmail.com>
2023-02-25 10:59:13 -0800

    update

---
+++ assets/DocDemo.69a5e0b4.js
@@ -0,0 +1,1 @@
+import{_ as e,o,ah as c}from"./index.baf8d374.js";const r={};function a(n,t){return o(),c("h1",null,"Demo")}var s=e(r,[["render",a]]);export{s as default};
---
+++ assets/DocLegal.3672176f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,ah as c}from"./index.baf8d374.js";const r={};function o(n,t){return a(),c("h1",null,"Legal")}var l=e(r,[["render",o]]);export{l as default};

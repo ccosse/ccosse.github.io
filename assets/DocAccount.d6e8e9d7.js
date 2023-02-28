cbfa9ff085d5b441df2268f8f3546d1c1239dfa0
ccosse <ccosse@gmail.com>
2023-02-28 01:50:48 -0800

    update

---
+++ assets/DocAccount.d6e8e9d7.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,an as n}from"./index.95bb4c97.js";const o={};function r(t,a){return e(),n("h1",null,"Account")}var s=c(o,[["render",r]]);export{s as default};
---
+++ assets/use-dark.c7b69132.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.95bb4c97.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};

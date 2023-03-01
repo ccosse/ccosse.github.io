d4978d1806886cb72939499fa702eb1d7be0b5bb
ccosse <ccosse@gmail.com>
2023-03-01 11:11:34 -0800

    update

---
+++ assets/DocAccount.bb69cd89.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,aq as o}from"./index.503ca5c1.js";const n={};function r(t,a){return e(),o("h1",null,"Account")}var s=c(n,[["render",r]]);export{s as default};
---
+++ assets/DocHistory.1e968d8c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,aq as o}from"./index.503ca5c1.js";const c={};function t(a,n){return r(),o("h1",null,"History")}var _=e(c,[["render",t]]);export{_ as default};

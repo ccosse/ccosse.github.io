a311729ec8f0bac9006f1ecec288b558c7ce5df1
ccosse <ccosse@gmail.com>
2023-04-28 12:50:48 -0700

    update

---
+++ assets/axios.81e7f998.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.f834d2d3.js";import{a}from"./axios.84ae42e4.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};

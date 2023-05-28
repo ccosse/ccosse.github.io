b539b78e464bf271d5c8502bade8adb3c9aa6c43
ccosse <ccosse@gmail.com>
2023-05-28 11:22:36 -0700

    update

---
+++ assets/use-timeout.f1e67e13.js
@@ -0,0 +1,1 @@
+import{a4 as r,o as i,a5 as s,g as a}from"./index.27556a2f.js";function l(){let e=null;const o=a();function t(){e!==null&&(clearTimeout(e),e=null)}return r(t),i(t),{removeTimeout:t,registerTimeout(n,u){t(),s(o)===!1&&(e=setTimeout(n,u))}}}export{l as u};
